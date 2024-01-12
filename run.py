2f2b9891af00676fc57a603fe5570e807aa03a80
ocicv <>
2024-01-12 17:35:32 +0000

    Functionality of the game

--- run.py
+++ run.py
@@ -34,3 +34,71 @@ def create_board_player():
     for loc in ships_locs:
         board[loc] = 'O'
     return board
+
+## How to dispay boards
+def display_board(board):
+    print('----------------------')
+    print('| ' + board['1A'] + ' | ' + board['2A'] + ' | ' + board['3A'] + ' | ' + board['4A'] + ' | ' + board['5A'] + ' |')
+    print('----------------------')
+    print('| ' + board['1B'] + ' | ' + board['2B'] + ' | ' + board['3B'] + ' | ' + board['4B'] + ' | ' + board['5B'] + ' |')
+    print('----------------------')
+    print('| ' + board['1C'] + ' | ' + board['2C'] + ' | ' + board['3C'] + ' | ' + board['4C'] + ' | ' + board['5C'] + ' |')
+    print('----------------------')
+    print('| ' + board['1D'] + ' | ' + board['2D'] + ' | ' + board['3D'] + ' | ' + board['4D'] + ' | ' + board['5D'] + ' |')
+    print('----------------------')
+    print('| ' + board['1E'] + ' | ' + board['2E'] + ' | ' + board['3E'] + ' | ' + board['4E'] + ' | ' + board['5E'] + ' |')
+    print('----------------------')
+    
+
+## function to play Battleship
+def game():
+    ## create boards for NPC and player
+    pc_board = create_board_computer()
+    player_board = create_board_player()
+    
+    ## display player's board
+    display_board(player_board)
+    
+    ## variables to control when the game ends
+    turn = 'Player'
+    pc_point = 6
+    player_point = 6
+    round = 0
+    
+    ## lists to keep track of player and NPC's moves
+    pc_moves = []
+    player_moves = []
+    
+    for i in range(25):
+        if turn == 'Player':
+            print('----------------------')
+            print('\033[1mPlayer turn:\033[0m')
+            move = input()
+            if move not in player_board.keys():
+                print('You hit a homerun! It\'s just not a good thing in this game.')
+            elif move in player_moves:
+                print('C\'mon! You hit it twice! That is cheating.')
+            else: 
+                if pc_board[move] == 'O':
+                    pc_point -= 1 
+                    print(f'Nice shot, Player. {pc_point} to go!')
+                else:
+                    print('Oh! That was a miss. Try harder next time.')
+            
+            player_moves.append(move)
+        
+        if turn == 'Computer':
+            print('----------------------')
+            print('\033[1mComputer turn:\033[0m')
+            move = random.sample(player_board.keys(), 1)
+            print(move[0])
+            if player_board[move[0]] == 'O':
+                player_point -= 1
+                print(f'Nice shot, Computer. {player_point} to go!')
+            elif move in pc_moves:
+                print('C\'mon! You hit it twice! That is cheating.')
+            else:
+                print('Oh! That was a miss. Try harder next time.')
+
+            pc_moves.append(move)
+            
